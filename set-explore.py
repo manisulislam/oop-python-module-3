ada3af3071231a1218e693c6e1c9a1547009e180
anisul islam <anisulislam980340@gmail.com>
2023-09-27 00:55:08 +0600

    set explore solved

---
+++ set-explore.py
@@ -0,0 +1,11 @@
+numbers={12,52,56,98,6,36}
+print(numbers)
+
+numbers_list=[14,25,52,65,14,21,14,85,96,14]
+numbers_set=set(numbers_list)
+print(numbers_set)
+
+numbers_set.add(896)
+print(numbers_set)
+numbers_set.remove(85)
+print(numbers_set)
