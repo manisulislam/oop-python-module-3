ea8bbbb3d347cef3a6b1c5c47f149f611a0a04f8
anisul islam <anisulislam980340@gmail.com>
2023-09-26 23:31:23 +0600

    string

---
+++ string-explore.py
@@ -0,0 +1,14 @@
+name1= 'anisul islam'
+name2= "anisul islam anis"
+name3= """
+    portcity international university
+"""
+print(name1)
+print(name2)
+
+
+print(name2[-3])
+print(name2[::-1])
+
+if "anis" in name2:
+    print("exists")
