9229fe60ad6d18b71182f0cba6440ee1f6c2f984
anisul islam <anisulislam980340@gmail.com>
2023-09-27 03:27:01 +0600

    file explore sovled

---
+++ file-explore.py
@@ -0,0 +1,11 @@
+# with open ("message.text", "w") as file:
+#     file.write("i love you, python!!")
+
+
+# with open ("message.text", "a") as file:
+#     file.write("i love you, python!!")
+
+with open ("message.text", "r") as file:
+    text = file.read()
+    print(text)
+    
